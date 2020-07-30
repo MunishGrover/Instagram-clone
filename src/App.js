f2e98c874edc5a0e37860d805d010fb4418ac810
MunishGrover <munishgrover98@gmail.com>
2020-07-31 02:28:20 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        />
+      </div>
       <h1>Hello after so many ages Im coding.</h1>
     </div>
   );
