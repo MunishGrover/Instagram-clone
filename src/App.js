f89fa2dedd4b82117cd49105f82317ed0f3e11b9
MunishGrover <munishgrover98@gmail.com>
2020-07-31 01:53:40 +0530

    template set

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello after so many ages Im coding.</h1>
+    </div>
+  );
+}
+
+export default App;
