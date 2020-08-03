6d1e9c2d27b8f268ff4333211967e30da5fe8777
MunishGrover <munishgrover98@gmail.com>
2020-08-03 23:21:29 +0530

    post added

--- src/Post.js
+++ src/Post.js
@@ -9,7 +9,7 @@ function Post() {
         src="https://images.unsplash.com/photo-1596280813649-7a07bc162282?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
       />
       <h4 className="post__text">
-        <strong>Munish: </strong>I love to vlog.
+        <strong>Munish: </strong>I love to vlog with these awesome cameras.
       </h4>
     </div>
   );
