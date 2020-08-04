81514461895464593f09641a11b7da2af1e5c033
MunishGrover <munishgrover98@gmail.com>
2020-08-04 20:38:44 +0530

    post

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "./Post.css";
+import Avatar from "@material-ui/core/Avatar";
 function Post() {
   return (
     <div className="post">
-      <h3>Username</h3>
+      <div className="post__header">
+        <Avatar
+          alt="Rahul"
+          className="post__avatar"
+          src="/static/images/avatar/1.jpg"
+        />
+        <h3>Username</h3>
+      </div>
       <img
         className="post__image"
         src="https://images.unsplash.com/photo-1596280813649-7a07bc162282?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
